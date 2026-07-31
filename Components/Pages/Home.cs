10b3af8515b99c11979169b955350c298a41c293
Sally Alkhalifah <saly.ali5858@gmail.com>
2026-07-31 10:45:51 +0300

    Add files via upload

---
+++ Components/Pages/Home.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp4.Pages
+{
+    public partial class Home
+    {
+        private int number = 0;
+        private void ChangeNumber()
+        {
+            number = new Random().Next(1, 100);
+        }
+
+    }
+}
---
+++ Components/Pages/Massage.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp4.Pages
+{
+    public partial class Massage
+    {
+        private string msg = "hello";
+        private bool isChanged = false;
+        private void ChangeMasseage()
+        {
+            msg = isChanged ? "opening massage" : "Massage has changed successfully";
+            isChanged = !isChanged;
+        }
+    }
+}
